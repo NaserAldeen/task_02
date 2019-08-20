f1377a7b5f3e5675a395b52172f8aa6f5aa54a50
NaserAldeen <30509@aum.edu.kw>
2019-08-20 12:09:45 +0300

    NaserAldeen Naser - Abddullah AlHasan

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def page_restaurant(request):
+    context= {
+    "msg": "Hello World!"
+    }
+    return render(request, 'page.html', context)
